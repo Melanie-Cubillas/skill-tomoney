bca54f2255bb91b4b40df605eef0f91cb4b318ff
Melanie Cubillas <melanie.cubillas@usil.pe>
2026-06-04 00:45:29 -0500

    Ajustes en registro

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -271,7 +271,7 @@ function Register() {
 
             <div className="grid grid-cols-2 gap-3">
               <div className="space-y-1.5">
-                <Label>Nombre</Label>
+                <Label>Nombres</Label>
                 <Input
                   placeholder={dniLookedUp ? "Camila" : "Valida tu DNI primero"}
                   value={firstName}
@@ -282,7 +282,7 @@ function Register() {
                 />
               </div>
               <div className="space-y-1.5">
-                <Label>Apellido</Label>
+                <Label>Apellidos</Label>
                 <Input
                   placeholder={dniLookedUp ? "Rojas" : "Valida tu DNI primero"}
                   value={lastName}
